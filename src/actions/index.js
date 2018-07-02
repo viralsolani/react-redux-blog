aab67d6e01fc573521d6d574bc7bad1fc7d1624a
Viral Solani <viral.solani@gmail.com>
2018-07-02 18:31:16 +0530

    Add new Post

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,27 @@
 import axios from "axios";
 
 export const FETCH_POSTS = "fetch_posts";
+export const CREATE_POST = "create_post";
 
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
-const API_KEY = "?key=PAPERCLIP1234";
+const API_KEY = "?key=Viral@1289";
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
   return {
     type: FETCH_POSTS,
+    payload: request
+  };
+}
+
+export function createPost(values, callback) {
+  const request = axios
+    .post(`${ROOT_URL}/posts${API_KEY}`, values)
+    .then(() => callback());
+
+  return {
+    type: CREATE_POST,
     payload: request
   };
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import { createPost } from "../actions";
 
 class PostsNew extends Component {
   renderField(field) {
@@ -20,7 +21,9 @@ class PostsNew extends Component {
   }
 
   onSubmit(values) {
-    console.log(values);
+    this.props.createPost(values, () => {
+      this.props.history.push("/");
+    });
   }
 
   render() {
