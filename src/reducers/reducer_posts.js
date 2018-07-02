32c91b533290671a8ae07cde626a8d2b57ef3d7d
Viral Solani <viral.solani@gmail.com>
2018-07-02 19:47:11 +0530

    Create API Call to fetch single post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -5,6 +5,12 @@ export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, "id");
+    case FETCH_POST:
+      // const post = action.payload.data;
+      // const newState = { ...state };
+      // newState[post.id] = post;
+      // return newState;
+      return { ...state, [action.payload.data.id]: action.payload.data };
     default:
       return state;
   }
