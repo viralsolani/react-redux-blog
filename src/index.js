3c5c31042b2bb7dbaa7e6b53d2f535462abb8867
Viral Solani <viral.solani@gmail.com>
2018-07-02 12:22:34 +0530

    list all posts from API

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import promise from "redux-promise";
 
 import reducers from "./reducers";
 import PostsIndex from "./components/posts_index";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/" component={PostsIndex} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.querySelector(".container")
 );
