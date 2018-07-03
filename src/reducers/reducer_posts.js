b1b1d414c12bb436ca237ad80516d7a2b5963c09
Viral Solani <viral.solani@gmail.com>
2018-07-03 06:08:41 +0530

    Delete Post Feature

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,10 @@
 import _ from "lodash";
-import { FETCH_POSTS, FETCH_POST } from "../actions";
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from "../actions";
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case DELETE_POST:
+      return _.omit(state, action.payload);
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, "id");
     case FETCH_POST:
